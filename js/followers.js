c7833e96dd623f69ede9f7b09be7b915ec049ea5
cxrvus <benito.m.rabe@gmail.com>
2024-03-21 20:28:16 +0100

    added followers whitelist feature

--- js/followers.js
+++ js/followers.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
 
-const DIR = `${__dirname}/../tmp/followers_and_following/`
+const TMP = `${__dirname}/../tmp`
+const DIR = `${TMP}/followers_and_following`
 
-const loadJson = path => JSON.parse(fs.readFileSync(DIR + path, 'utf8'))
+const getWhiteList = () => {
+	const WL_PATH = `${TMP}/whitelist.txt`
+	if (fs.existsSync(WL_PATH)) return fs.readFileSync(WL_PATH, 'utf-8').split('\n').filter(x => x)
+	else return []
+}
+
+const loadJson = path => JSON.parse(fs.readFileSync(`${DIR}/${path}`, 'utf8'))
 const getValue = obj => obj.string_list_data[0].value
 
-const followers = loadJson('followers_1.json').map(x => getValue(x))
+const followers = loadJson('followers_1.json').map(x => getValue(x)).concat(getWhiteList())
 const following = loadJson('following.json').relationships_following.map(x => getValue(x))
 
 const notFollowingBack = following.filter(follower => !followers.includes(follower))
