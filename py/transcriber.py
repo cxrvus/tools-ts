fc973228ee22818472bc554ccf6cdff83f4b7e76
cxrvus <benito.m.rabe@gmail.com>
2024-04-12 15:32:16 +0200

    fixed and progressed with transcriber.py

--- py/transcriber.py
+++ py/transcriber.py
@@ -3,10 +3,6 @@ import sys
 import whisper
 
 
-model = whisper.load_model('base')
-options = whisper.DecodingOptions(language='en', fp16=False)
-
-
 if len(sys.argv) < 2:
 	print("<!> directory not provided")
 	sys.exit(1)
@@ -19,19 +15,28 @@ if not path or not os.path.isdir(path):
 
 folder_name = path.split('/')[-1]
 
-all_files = os.listdir(path)
+all_files = sorted(os.listdir(path))
 files = list(filter(lambda file: file.lower().endswith('.wav'), all_files))
 file_count = len(files)
 
+
+model = whisper.load_model('base')
+
 transcript = ''
+outfile = os.path.join(path, folder_name) + '.txt'
+
+print('transcribing...')
+
+with open(outfile, 'w') as file:
+	file.write('')
 
 for (i, file) in enumerate(files):
-	print(f'transcribing... ({i+1:02}/{file_count:02})')
+	print(f'({i+1:02}/{file_count:02})')
 	audio = whisper.load_audio(os.path.join(path, file))
-	result = whisper.transcribe(model, audio, options)
+	result = whisper.transcribe(model, audio, language='en', fp16=False)	
 	transcript += f'## {file}\n\n'
 	transcript += f'{result["text"]}\n\n\n'
-	with open(folder_name + '.txt', 'w') as file:
+	with open(outfile, 'w') as file:
 		file.write(transcript)
 
 print('done!')
