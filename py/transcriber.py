209836e87670e65275a9962ec951fc670766a2e9
cxrvus <benito.m.rabe@gmail.com>
2024-05-04 16:19:22 +0200

    transcriber: added support for arbitrary audio file names

--- py/transcriber.py
+++ py/transcriber.py
@@ -30,6 +30,17 @@ model = whisper.load_model('base')
 transcript = ''
 outfile = os.path.join(path, '_' + folder_name) + '.md'
 
+
+def get_header(file):
+	file_name = '.'.join(file.split('.')[:-1])
+	header_content = ''
+	try:
+		header_content = int(file_name[3:]) # numerical index as header
+	except:
+		header_content = file_name # file name as header
+	return f'## {header_content}\n\n'	
+
+
 print('transcribing...')
 
 with open(outfile, 'w') as file:
@@ -42,9 +53,8 @@ for (i, file) in enumerate(files):
 	audio = whisper.load_audio(os.path.join(path, file))
 	result = whisper.transcribe(model, audio, language=lang, fp16=False)	
 	text = result['text']
-	index = int(file.split('.')[0][3:])
 
-	transcript += f'## {index}\n\n'
+	transcript += get_header(file)
 	transcript += f'![[{file}]]\n\n'
 	transcript += f'{text}\n\n'
 	transcript += '\n'
