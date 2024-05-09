4baef227a5621d4c1b945aed6169bfe7547a4dbc
cxrvus <benito.m.rabe@gmail.com>
2024-05-09 14:19:12 +0200

    added follower and following count

--- js/followers.js
+++ js/followers.js
@@ -19,13 +19,18 @@ const getWhiteList = () => {
 const loadJson = (dir, path) => JSON.parse(tryReadFile(`${dir}/${path}`)) ?? []
 const getValue = obj => obj.string_list_data[0].value
 
-const followers = loadJson(DIR, 'followers_1.json').map(x => getValue(x)).concat(getWhiteList())
+const actual_followers = loadJson(DIR, 'followers_1.json').map(x => getValue(x))
+const follower_count = actual_followers.length
+const followers = actual_followers.concat(getWhiteList())
 const following = loadJson(DIR, 'following.json').relationships_following.map(x => getValue(x))
+const following_count = following.length
 
 const notFollowingBack = following.filter(follower => !followers.includes(follower))
 
 const old_followers = loadJson(ARCHIVE, 'followers_1.json').map(x => getValue(x)).concat(getWhiteList())
 const unfollowed = old_followers.filter(x=>!followers.includes(x))
+
+console.log(`---- Followers: ${follower_count} | Following: ${following_count} ----`)
 
 if (notFollowingBack.length) {
 	console.log(`-- Not Following You Back (${notFollowingBack.length}) --`)
